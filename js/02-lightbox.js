8bc77cc32a321ac9d5561fad27aa9f84f69b8ca9
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-04-02 13:27:17 +0200

    doing 02 lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+(function () {
+  const smallImgGallery = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+    })
+    .join("");
+  gallery.insertAdjacentHTML("afterbegin", smallImgGallery);
+})();
+
+const onSmallImgClick = (e) => {
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery .gallery__link", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: "250",
+  });
+};
+
+gallery.addEventListener("click", onSmallImgClick);
